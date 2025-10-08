4550fbafc0d370875b13e84fb1f6ef0bf6c8476d
Jesse Peterson <jesse.c.peterson@gmail.com>
2025-10-07 23:48:37 -0700

    add var test

--- envflag/envflag_test.go
+++ envflag/envflag_test.go
@@ -5,6 +5,19 @@ import (
 	"testing"
 )
 
+type stringFlag struct {
+	str string
+}
+
+func (f *stringFlag) String() string {
+	return f.str
+}
+
+func (f *stringFlag) Set(s string) error {
+	f.str = s
+	return nil
+}
+
 func TestEnvFlag(t *testing.T) {
 
 	fs := flag.NewFlagSet("test", flag.ContinueOnError)
@@ -14,11 +27,14 @@ func TestEnvFlag(t *testing.T) {
 	testStr3 := fs.String("str-3", "", "use-3")
 	testStr4 := fs.String("str-4", "def-4", "use-4")
 	testBol5 := fs.Bool("bol-5", false, "use-5")
+	testBol6 := &stringFlag{}
+	fs.Var(testBol6, "str-6", "use-6")
 
 	env := []string{
 		"TEST_STR_1=env-1", // "-str-1"
 		"TEST_STR_4=env-4", // "-str-4"
 		"TEST_BOL_5=true",  // "-bol-5"
+		"TEST_STR_6=env-6", // "-str-6"
 	}
 
 	err := PreParseFlagSet(fs, "TEST_", env, []string{"str-5"})
@@ -57,6 +73,11 @@ func TestEnvFlag(t *testing.T) {
 	// no args, has envvar, no default: should be use envvar (bool test)
 	if have, want := *testBol5, true; have != want {
 		t.Errorf("have: %t, want: %t", have, want)
+	}
+
+	// no args, has envvar, no default: should be use envvar (var test)
+	if have, want := testBol6.str, "env-6"; have != want {
+		t.Errorf("have: %q, want: %q", have, want)
 	}
 
 }
