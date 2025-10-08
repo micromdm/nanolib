118554e0dd99fd039a78f8948929c15d2870516c
Jesse Peterson <jesse.c.peterson@gmail.com>
2025-10-07 23:07:38 -0700

    add envflag

---
+++ envflag/envflag_test.go
@@ -0,0 +1,76 @@
+package envflag
+
+import (
+	"flag"
+	"testing"
+)
+
+func TestEnvFlag(t *testing.T) {
+
+	fs := flag.NewFlagSet("test", flag.ContinueOnError)
+
+	testStr1 := fs.String("str-1", "def-1", "use-1")
+	testInt2 := fs.Int64("int-2", 2, "use-2")
+	testStr3 := fs.String("str-3", "", "use-3")
+	testStr4 := fs.String("str-4", "def-4", "use-4")
+	testBol5 := fs.Bool("bol-5", false, "use-5")
+
+	env := []string{
+		"TEST_STR_1=env-1", // "-str-1"
+		"TEST_STR_4=env-4", // "-str-4"
+		"TEST_BOL_5=true",  // "-bol-5"
+	}
+
+	err := PreParseFlagSet(fs, "TEST_", env, []string{"str-5"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	args := []string{"-str-4", "arg-4"}
+
+	err = fs.Parse(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// fs.Usage()
+
+	// no args, has envvar, has default: should use envvar (envvar overrides default, but not args)
+	if have, want := *testStr1, "env-1"; have != want {
+		t.Errorf("have: %q, want: %q", have, want)
+	}
+
+	// no args, no envvar, has default: should use default
+	if have, want := *testInt2, int64(2); have != want {
+		t.Errorf("have: %q, want: %q", have, want)
+	}
+
+	// no args, no envvar, no default: should be empty
+	if have, want := *testStr3, ""; have != want {
+		t.Errorf("have: %q, want: %q", have, want)
+	}
+
+	// has args, has envvar, has default: should be args (args overrides all)
+	if have, want := *testStr4, "arg-4"; have != want {
+		t.Errorf("have: %q, want: %q", have, want)
+	}
+
+	// no args, has envvar, no default: should be use envvar (bool test)
+	if have, want := *testBol5, true; have != want {
+		t.Errorf("have: %t, want: %t", have, want)
+	}
+
+}
+
+func TestNameToEnv(t *testing.T) {
+	for k, v := range map[string]string{
+		"name":      "NAME",
+		"with-dash": "WITH_DASH",
+		"o":         "O",
+		"one1":      "ONE1",
+		".":         "_",
+	} {
+		if have, want := nameToEnv(k), v; have != want {
+			t.Errorf("for: %q, want: %q, have: %q", k, want, have)
+		}
+	}
+}
